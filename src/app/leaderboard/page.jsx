bb2c29ce6fd229f2893610a962bb625f2d27479e
Durjoy96 <mddurjoy853@gmail.com>
2025-09-16 23:49:14 +0600

    add title and description for the leaderboard component

--- src/app/leaderboard/page.jsx
+++ src/app/leaderboard/page.jsx
@@ -21,13 +21,23 @@ export default async function Leaderboard() {
 
   return (
     <div data-theme="forest" className="min-h-screen bg-base-200 font-inter">
-      <div className="max-w-6xl mx-auto px-5">
-        <div className="flex justify-between gap-6 pt-12">
+      <div className="max-w-6xl mx-auto px-5 pt-12">
+        <div>
+          <h2 className="text-4xl font-semibold font-fredoka">
+            Can you beat them?
+          </h2>
+          <p className="text-base text-base-content/70 max-w-2xl mt-2">
+            The hall of fame for people who mastered the art of absolutely
+            nothing.
+          </p>
+        </div>
+        {/* tables */}
+        <div className="flex justify-between gap-6 pt-8">
           {/* This Week */}
           <div>
             {/* title */}
             <div className="flex items-center gap-3 mb-3">
-              <h2 className="text-lg font-bold">This Week</h2>
+              <h3 className="text-lg font-bold">This Week</h3>
               <span class="relative flex size-3">
                 <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-primary opacity-75"></span>
                 <span class="relative inline-flex size-3 rounded-full bg-primary"></span>
@@ -40,7 +50,7 @@ export default async function Leaderboard() {
           <div>
             {/* title */}
             <div className="mb-3">
-              <h2 className="text-lg font-bold">All-Time</h2>
+              <h3 className="text-lg font-bold">All-Time</h3>
             </div>
             {/* table */}
             <LeaderboardTable data={allTimeData.allTimeTopPlayers} />
